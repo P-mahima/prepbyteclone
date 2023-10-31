bf9b12ac05baa35950ff665ac3586e6387b3d695
mahima <pawar.mahima23@gmail.com>
2023-10-31 11:45:52 +0530

    commit

--- route/route.js
+++ route/route.js
@@ -2,7 +2,6 @@ const route = require('express').Router()
 const { RegisterUser, LoginUser } = require('../controller/userController')
 
 
-// const {Register,LoginUser} = require('../controller/usercontroller')
 
 
 route.post('/register', RegisterUser)
